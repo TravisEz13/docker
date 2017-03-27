c6499000bb2b89c366afe8f45ac5741e9e19b200
Travis Plunk <travis.plunk@microsoft.com>
2017-03-26 17:05:13 -0700

    make infrastructure more generic

---
+++ microsoft/windowsservercore/git/git.tests.ps1
@@ -0,0 +1,17 @@
+        Describe "travisez13/microsoft.windowsservercore.git:latest" {
+            $repos = @(
+                @{repo = 'travisez13/microsoft.windowsservercore.git'}
+                @{repo = 'travisez13/microsoft.windowsservercore.git_and_nuget'}
+            )
+
+            it "should contain git" -TestCases $repos {
+                params(
+                    [parameter(Mandatory)]
+                    [string]$repo
+                )
+
+                start-process -Wait -filepath docker -argumentlist @('run', "${repo}:latest", 'git', 'version') -PassThru  -RedirectStandardError Testdrive:\stderr.txt -RedirectStandardOutput testdrive:\stdout.txt 
+                Get-Content testdrive:\stderr.txt | should benullorempty
+                Get-Content testdrive:\stdout.txt | should belike 'git version *.windows.*'
+            }
+        }
